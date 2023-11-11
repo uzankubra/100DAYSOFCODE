59537092446147de758729b8c1a6f7d93c4c3f64
Kübra Uzan <63738480+uzankubra@users.noreply.github.com>
2023-11-11 16:36:02 +0300

    Add files via upload

---
+++ 2_day21/food.py
@@ -0,0 +1,21 @@
+
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.color("blue")
+        self.shapesize(0.5,0.5)
+        self.speed("fastest")
+        random_x= random.randint(-280,280)
+        random_y= random.randint(-280,280)
+        self.goto(random_x,random_y)
+
+    def refresh(self):
+        random_x= random.randint(-280,280)
+        random_y= random.randint(-280,280)
+        self.goto(random_x,random_y)
+
