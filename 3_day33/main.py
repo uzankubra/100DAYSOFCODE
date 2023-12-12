22ac4e6ab3017770005a35cf9bb860f5cb5c6180
Kübra Uzan <63738480+uzankubra@users.noreply.github.com>
2023-12-12 15:34:58 +0300

    Add files via upload

--- 3_day33/main.py
+++ 3_day33/main.py
@@ -1,19 +1,55 @@
 import requests
+from datetime import datetime
+import smtplib
+import time
 
-def get_iss_location():
-    api_url = "http://api.open-notify.org/iss-now.json"
+MY_EMAIL = "protocoltestmail2@gmail.com"
+MY_PASSWORD = "km2002()"
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # tolerans
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
 
 
-    response = requests.get(api_url)
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
 
-    # check
-    if response.status_code == 200:
-        data = response.json()
-        latitude = data['iss_position']['latitude']
-        longitude = data['iss_position']['longitude']
-        print(f"ISS'nin güncel konumu: Enlem {latitude}, Boylam {longitude}")
-    else:
-        print(f"Hata: {response.status_code}")
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
 
-get_iss_location()
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smpt.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+        )
+
+
