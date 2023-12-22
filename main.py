3a45f89931bebf82c1fcd1e7f9d4752a0dcf84b3
Kübra Uzan <63738480+uzankubra@users.noreply.github.com>
2023-12-23 02:32:32 +0300

    weather api
    api request

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import requests
+from twilio.rest import Client
+import os
 
-owm_endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
-api_key="3f6c191ebc22ffb1b1d15dabeafff115"
+URL="http://api.openweathermap.org/data/2.5/forecast"
+API_KEY="3f6c191ebc22ffb1b1d15dabeafff115"
 
-weather_params={
-    "lat": 41.008240,
-    "lon": 28.978359,
-    "appid": api_key
+TWILIO_KEY="H2UR23FYP4451X191VH8CT7J"
+
+LAT=41.008240
+LON=28.978359
+
+params_weather={
+    "lat":LAT,
+    "lon":LON,
+    "appid":API_KEY,
+    "cnt":4
 }
-response=requests.get(owm_endpoint, params=weather_params)
-print(response.status_code)
+
+response=requests.get(URL,params=params_weather)
+
+# print(response.status_code)
+weather_data=response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
 
 
+account_sid = 'AC40a447f94ce52da3e5cb5bbea3842f52'  # it should be your accound_sid, it is not gonna work
+auth_token = '12417009cf872bef0cc630cb43580dfd' # it should be your auth_token
 
+will_rain=False
+for hour_data in weather_data["list"]:
+    condition= hour_data["weather"][0]["id"]
 
+    if int(condition)<900:
+        will_rain=True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+        from_='+19283797635',
+        to='####your phone number###'
+    )
+
+    print(message.status)
 
